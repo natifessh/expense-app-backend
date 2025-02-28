d5409d5a3cb190dab38282348fe38e5e43d4f665
nati_fessh <95565112+natifessh@users.noreply.github.com>
2025-02-28 21:30:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use((req, res, next) => {
 });
 app.use('/expenses', expenseRouter);
 app.use("/auth", authRoutes);
-app.listen(3001, () => {
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
